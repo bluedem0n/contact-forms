116064771c353e1b5dab54b39df4b47f8bd25390
Sthefany <sthefanyflorianog@gmail.com>
2016-12-03 15:59:01 -0500

    jquerys

--- funciones.js
+++ funciones.js
@@ -1,42 +1,105 @@
-(function(){
-    $(".boton_envio").click(function() {
+function cargaSendMail(){
  
-        var nombre = $(".nombre").val();
-            email = $(".email").val();
-            validacion_email = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
-            telefono = $(".telefono").val();
-            mensaje = $(".mensaje").val();
+
+    $(".c_error").remove();
  
-        if (nombre == "") {
-            $(".nombre").focus();
-            return false;
-        }else if(email == "" || !validacion_email.test(email)){
-            $(".email").focus();    
-            return false;
-        }else if(telefono == ""){
-            $(".telefono").focus();
-            return false;
-        }else if(mensaje == ""){
-            $(".mensaje").focus();
-            return false;
-        }else{
-            $('.ajaxgif').removeClass('hide');
-            var datos = 'nombre='+ nombre + '&email=' + email + '&telefono=' + telefono + '&mensaje=' + mensaje;
-            $.ajax({
-                type: "POST",
-                url: "proceso.php",
-                data: datos,
-                success: function() {
-                    $('.ajaxgif').hide();
-                    $('.msg').text('Mensaje enviado!').addClass('msg_ok').animate({ 'right' : '130px' }, 300);  
-                },
-                error: function() {
-                    $('.ajaxgif').hide();
-                    $('.msg').text('Hubo un error!').addClass('msg_error').animate({ 'right' : '130px' }, 300);                 
-                }
-            });
-            return false;
-        }
+	var filter= /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+    var s_email = $('#c_mail').val();
+    var s_name = $('#c_name').val();    
+    var s_msg = $('#c_msg').val();
+	var s_spam_textbox1 = $('#c_spam_textbox1').val();
+	var s_spam_textbox2 = $('#c_spam_textbox2').val();
+	
+	
+    if (filter.test(s_email)){
+    sendMail = "true";
+	
+    $('#c_mail').css("border-color","");   	
+    } else{
+    $('#c_mail').after("<span class='c_error' id='c_error_mail'>Ingrese e-mail valido.</span>");
+	$(".c_error").css("color","Red");
+    $('#c_mail').css("border-color","Red");   
+    sendMail = "false";
+    }
+    if (s_name.length == 0 ){
+    $('#c_name').after( "<span class='c_error' id='c_error_name'>Ingrese su nombre.</span>" );
+	$(".c_error").css("color","Red");
+	$('#c_name').css("border-color","Red");  	
+    var sendMail = "false";
+    } else{
+	$('#c_name').css("border-color","");
+    }	
+    if (s_msg.length == 0 ){
+    $('#c_msg').after( "<span class='c_error' id='c_error_msg'>Ingrese mensaje.</span>" );
+	$(".c_error").css("color","Red");
+	$('#c_msg').css("border-color","Red");
+    var sendMail = "false";
+    } else{
+	$('#c_msg').css("border-color","");
+    }		
+	//Si el primer textbox se deja en blanco
+    if (s_spam_textbox1.length == 0 ){
+	    var s_Bot1 = "false";
+    } 
+    //Si el segundo textbox no se modifica
+	if (s_spam_textbox2 == "http://" ){
+		var s_Bot2 = "false";
+    }
+	if (s_Bot1 == "false" && s_Bot2 == "false"){
+		spamBot = "false";
+	}
+	else { spamBot = "true"; }
+
+	
+    
+    if(sendMail == "true" && spamBot == "false" ){
+     
+     var datos = {
  
-    });
-})();
+            "nombre" : $('#c_name').val(),
+ 
+            "email" : $('#c_mail').val(),
+			 
+			"url" : $('#c_url').val(),
+						  
+			"telefono" : $('#c_telefono').val(),
+ 
+            "mensaje" : $('#c_msg').val()
+			 
+     };
+ 
+     $.ajax({
+ 
+             data:  datos,
+             // hacemos referencia al archivo mail.php
+             url:   'mail.php',
+ 
+             type:  'post',
+ 
+             beforeSend: function () {
+ 
+                    $("#c_enviar").val("Enviando...");
+ 
+             },
+ 
+             success:  function (response) {
+ 
+                    $('form')[0].reset(); 
+                    $("#c_enviar").val("Enviar Mensaje");
+                    $("#c_information p").html(response);
+					$("#c_information").css({
+											"background-color": "#DFF2BF",
+											"color": "#4F8A10",
+											"background-image": "url('img/exito.png')"
+					});				
+					$("#c_information").text( "¡Mensaje enviado correctamente!" );
+                    $("#c_information").fadeIn('slow');
+					
+ 
+             }
+	 
+     });
+ 
+	}
+ 
+}
---
+++ mail.php
@@ -0,0 +1,44 @@
+
+<?php
+
+
+
+if(isset($_POST['cenviar'])){
+ 
+ 
+    if(isset($_POST['nombre'])){
+        $nombre = $_POST['nombre'];
+    }
+    if(isset($_POST['email'])){
+        $email = $_POST['email'];
+    }
+    if(isset($_POST['url'])){
+        $url = $_POST['url'];
+    }
+    if(isset($_POST['telefono'])){
+        $telefono = $_POST['telefono'];
+    }
+    if(isset($_POST['mensaje'])){
+        $mensaje = $_POST['mensaje'];
+    }
+    // Si cualquier línea es más larga de 70 caracteres, se debería usar wordwrap()
+    $mensaje = wordwrap($mensaje, 70, "\r\n");
+ 
+ 
+ 
+    $msj = "De: ".$nombre."\r\n";
+    $msj .= "Email: ".$email."\r\n";
+    $msj .= "Fecha: ".date("d-m-Y H:i:s")."\r\n";
+    $msj .= "Web: ".$url."\r\n";
+    $msj .= "Telefono: ".$telefono."\r\n\n\n\n";
+    $msj .= "Mensaje: ".$mensaje;
+ 
+ 
+    mail('sthefanyflorianog@codespacelab.com', 'Asunto: Probando formulario de contacto...', $msj);
+ 
+}
+else die("El acceso directo a este archivo no es permitido.");
+
+	
+	
+?>
